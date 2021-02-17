e792ba1581018bb2c809d997213b29c9a6808b83
unknown <tankovic.lukas@yahoo.com>
2021-02-17 15:10:33 +0100

    init

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'azuresite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgreSQL',
+        'NAME': BASE_DIR / 'db.postgreSQL',
     }
 }
 
